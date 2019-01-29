b3cc3096648d3a83d32a7b81afb202a10fa5baa7
James <42189301+James-Is-A-Name@users.noreply.github.com>
2019-01-29 17:21:06 +1300

    add simple title for each person thread

--- src/components/MessageThread.jsx
+++ src/components/MessageThread.jsx
@@ -7,8 +7,15 @@ function MessageThread(props){
         return <li key={`message-id-${i}`}> <Message message={message}/> </li>
     });
     
+    let personTitle = <h2>Unknown user</h2>;
+    if(props.messages.length > 0){
+        if(props.messages[0])
+        personTitle = <h2>Person {props.messages[0].id}</h2>
+    }
+
     return (
         <div style={{border:"solid 1px blue", padding:"5px", margin:"2px"}}>
+            {personTitle}
             <ul>
                 {theMessages}
             </ul>
